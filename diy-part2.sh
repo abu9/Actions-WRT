35d3e16e4435fe3b17d30cc416f1b4093427cc08
abu9 <abu9@users.noreply.github.com>
2021-11-05 03:06:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,3 +16,5 @@ curl -fsSL git.io/file-transfer | sh
 echo 'src-git kenzojell src-git kenzojell https://github.com/kenzok8/jell' >>feeds.conf.default
 ./scripts/feeds update kenzojell
 ./scripts/feeds install luci-app-vlmcsd luci-app-zerotier kmod-crypto-hw-eip93
+sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
+sed -i 's/radio\${devidx}\.disabled=1/radio\${devidx}\.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
