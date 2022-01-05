f5fee28f62e7200a385679249c942bf5b5e58530
abu9 <abu9@users.noreply.github.com>
2022-01-05 11:08:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,8 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-./scripts/feeds install -a -p zerotier -f
+./scripts/feeds uninstall zerotier
+./scripts/feeds install -a -p zerotier -f -d y
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 curl -fsSL git.io/file-transfer | sh
