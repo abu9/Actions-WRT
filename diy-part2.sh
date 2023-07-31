dbd3a99f6193519f3f048ca70bc6d41614f731ee
abu9 <abu9@users.noreply.github.com>
2023-08-01 01:43:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,8 @@
 echo 'src-git jell https://github.com/kenzok8/jell' >>feeds.conf.default
 ./scripts/feeds update jell
 ./scripts/feeds uninstall luci-app-fileassistant
-./scripts/feeds install -p jell -f -d y luci-app-fileassistant luci-app-adguardhome luci-app-shadowsocks-rust
-./scripts/feeds uninstall libffi
+./scripts/feeds install -p jell -f -d y luci-app-fileassistant luci-app-adguardhome
+#./scripts/feeds uninstall libffi
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 # curl -fsSL git.io/file-transfer | sh
