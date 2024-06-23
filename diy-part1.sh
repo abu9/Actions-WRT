071efb10a563dadc8dccd9c540c346e96f1e24c9
abu9 <2047272+abu9@users.noreply.github.com>
2024-06-23 18:39:09 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,36 @@
 # Add a feed source
 # echo 'src-git kenzojell https://github.com/kenzok8/jell' >>feeds.conf.default
 
+function git_sparse_clone() (
+	branch="$1" rurl="$2" localdir="$3" && shift 3
+	git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$rurl" "$localdir"
+	cd "$localdir" || exit
+	git sparse-checkout init --cone
+	git sparse-checkout set "$@"
+	mv -n "$@" ../
+	cd ..
+	rm -rf "$localdir" 
+	)
+
+function git_clone() (
+	git clone --depth 1 --filter=blob:none "$1"
+	)
+
+custom_feed_path="$PWD/customfeed"
+[[ -s feeds.conf.default ]] && sed -i "1isrc-link custom $custom_feed_path" feeds.conf.default
+
+mkdir "$custom_feed_path" && cd "$_" || exit
+git_sparse_clone master "https://github.com/immortalwrt/packages" "immpkgs" lang/perl #fix perl compile issue with external-toolchain
+git_sparse_clone master "https://github.com/immortalwrt/luci" "immlucipkgs" applications/luci-app-filebrowser
+
+git_clone https://github.com/tty228/luci-app-wechatpush 
+git_clone https://github.com/stevenjoezhang/luci-app-adguardhome
+git_clone https://github.com/gSpotx2f/luci-app-log
+
+
+case $REPO_URL in
+    immortalwrt/immortalwrt)
+        git apply "${GITHUB_WORKSPACE}"/patchs/0001-fix-default-settings-depends.patch
+		git_sparse_clone master "https://github.com/openwrt/packages" "officialpkgs" libs/libffi 		#fix libffi compile issue with external-toolchain
+        ;;
+esac
--- diy-part2.sh
+++ diy-part2.sh
@@ -9,33 +9,15 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-function merge_package(){
-    repo=$(echo "$1" | rev | cut -d'/' -f 1 | rev)
-    pkg=$(echo "$2" | rev | cut -d'/' -f 1 | rev)
-    find package/ -follow -name "$pkg" -not -path "package/custom/*" -print0 | xargs -0 -rt rm -rf
-    git clone --depth=1 --single-branch "$1"
-    mv "$2" package/custom/
-    rm -rf "$repo"
-}
-function drop_package(){
-    find package/ -follow -name "$1" -not -path "package/custom/*" -print0 | xargs -0 -rt rm -rf
-}
 
-echo 'src-git jell https://github.com/kenzok8/jell' >>feeds.conf.default
-echo 'src-git immortal_pkg https://github.com/immortalwrt/packages' >>feeds.conf.default
-./scripts/feeds update
-./scripts/feeds uninstall luci-app-fileassistant zerotier cloudflared golang
-./scripts/feeds install -p immortal_pkg -f golang
-./scripts/feeds install -p jell -f -d y luci-app-fileassistant luci-app-adguardhome luci-app-log
-./scripts/feeds install -p immortal_pkg -f -d y zerotier cloudflared
-#./scripts/feeds uninstall libffi
+
+# ./scripts/feeds update
+# ./scripts/feeds uninstall luci-app-fileassistant zerotier cloudflared golang
+# ./scripts/feeds install -p immortal_pkg -f golang zerotier cloudflared luci-app-filebrowser
+# ./scripts/feeds install -p jell -f luci-app-fileassistant luci-app-adguardhome luci-app-log
+
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-# curl -fsSL git.io/file-transfer | sh
 sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
-# sed -i 's/radio\${devidx}\.disabled=1/radio\${devidx}\.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-#git apply files/enable-vlan-for-mtwifi.patch
-# patch -p0 -i files/fix-libffi-build.patch
-#patch -p0 -i files/remove-luci-uhttpd.patch
-#git apply files/Kmemleak.patch
-#rm target/linux/generic/pending-5.4/761-net-dsa-mt7530-Support-EEE-features.patch
+
+#fix vlmcsd compile issue with ccache and external-toolchain
+patch -p1 -d feeds/packages <../patchs/vlmcsd.patch
